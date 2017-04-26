e3684b95826e57e9c58de34ab5b2ab77361c4c0e
liangning001 <liangning001@gmail.com>
2017-04-26 17:09:02 +0800

    step 6 order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,15 +4,15 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
   def index
-   @jobs = case params[:order]
-           when 'by_lower_bound'
-             Job.published.order('wage_lower_bound DESC')
-           when 'by_upper_bound'
-             Job.published.order('wage_upper_bound DESC')
-           else
-             Job.published.recent
-           end
- end
+    @jobs = case params[:order]
+            when 'by_lower_bound'
+              Job.published.order('wage_lower_bound DESC')
+            when 'by_upper_bound'
+              Job.published.order('wage_upper_bound DESC')
+            else
+              Job.published.recent
+            end
+  end
  def new
     @job = Job.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   namespace :admin do
-      resources :jobs
+    resources :jobs do
+  resources :resumes
 
+  member do
+  post :publish
+  post :hide
+  end
+  end
    root 'welcome#index'
    root 'jobs#index'
    resources :jobs
